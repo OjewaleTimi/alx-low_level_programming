e562c18e981582d719b1675c2cf3d0bcc5526d33
OjewaleTimi <mitytimmy@gmail.com>
2022-09-09 00:30:40 +0100

    First C files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+*main-program prints with function puts
+*
+*Return: Always 0
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual language");
+    return(0);
+}

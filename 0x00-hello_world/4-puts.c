18df306e055522ff72bb6916bc47003ece87d0ed
OjewaleTimi <mitytimmy@gmail.com>
2022-09-09 01:41:19 +0100

    First C files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
 /**
-*main-program prints with function puts
-*
-*Description : A C program that uses put function
-*
-*Return: Always 0
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual language");
-    return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,16 @@
- #include <stdio.h> 
+#include<stdio.h>
 
- /** 
- *main - print string 
- * 
- *return: always 0 
- */ 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
 
- int main(void) 
- { 
-    printf("with proper grammar, but the outcome is a piece of art,\n"); 
-    return (0); 
- }
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+

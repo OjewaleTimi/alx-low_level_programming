6a0c28d4655df85d921fc5ef7e92719dabd553ab
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-09-09 23:43:33 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 /*betty style for function main goes there*/
@@ -12,7 +13,7 @@ int main(void)
 {
     int n; 
         
-    srand(time(0));
+    srand (time(0));
     n = rand() - RAND_MAX / 2;
     if (n > 0)
     {printf ("%i is positive/n",n);}

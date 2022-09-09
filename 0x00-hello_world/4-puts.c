be11ed1fe850cf4ee0b12135d83e73f572119342
OjewaleTimi <mitytimmy@gmail.com>
2022-09-09 01:27:06 +0100

    First C files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,11 @@
 /**
 *main-program prints with function puts
 *
+*Description : A C program that uses put function
+*
 *Return: Always 0
 */
+
 int main(void)
 {
     puts("\"Programming is like building a multilingual language");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
  #include <stdio.h> 
+
  /** 
- *main - print string * 
+ *main - print string 
+ * 
  *return: always 0 
  */ 
+
  int main(void) 
  { 
     printf("with proper grammar, but the outcome is a piece of art,\n"); 

55c8a718992d0ba0ebe9fee81ec9af28c0576bac
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-09-23 00:09:06 +0100

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0   */
+char *_strcat(char *dest, char *src)
+{
+int i = 0 ;
+int destLength = 0;
+while(dest[i++]){
+  
+destLength++;
+for (i = 0,src[i],i++)
+dest[destLength++] = src[i];
+ return dest;
+
+} 

a465cf47abadc85569402dc91d41f89ce8515fed
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-09-24 00:19:33 +0100

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+/**
+ *void prototypes()
+ *int prototypes()
+ *char prototypes()
+ */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -6,3 +14,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+void print_number(int n);
+void print_buffer(char *b, int size);
+#endif /*MAIN_H*/

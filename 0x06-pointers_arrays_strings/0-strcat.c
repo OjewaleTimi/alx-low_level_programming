9768559778216019bae8a8a29c0cab7ef61a19d8
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-09-24 00:21:06 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,15 @@
 
 char *_strcat(char *dest, char *src)
 {
-int i = 0 ;
-int destLength = 0;
-while(dest[i++]){
-  
-destLength++;
-for (i = 0, src[i] , i++)
-    dest[destLength++] = src[i];
-    return dest;
 
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
 } 

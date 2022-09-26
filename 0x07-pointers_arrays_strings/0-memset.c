64f072ef268c0632ab3b93608fa4e712f698c2b2
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-09-26 23:45:42 +0100

    Create 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*memset- fills byte to a constant
+*char *s- prints the address of the variable
+*unsigned int (n)
+*
+Return: a pointer to the memory area @s
+**/
+ char *_memset(char *s, char b, unsigned int n){
+      unsigned int n;
+     for(i=0; i < n ; i++){
+     s[i] = b;
+     }
+     return (s);
+     

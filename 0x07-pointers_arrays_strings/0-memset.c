dc9b105d0cdee4dc1cd8bffac8d06f5fd5829b85
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-09-27 00:27:12 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,15 +8,15 @@
 Return: a pointer to the memory area @s
 */
 
- char *_memset(char *s, char b, unsigned int n)
- {
-      unsigned int i;
-      
-     for(i = 0; n > 0 ; i++,n--)
- `   {
-           s[i] = b;
-     }
-     
-     return (s);
-     
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }

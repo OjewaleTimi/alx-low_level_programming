977b8c3828acca201475342f40b2a212fa83d245
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-09-28 22:49:22 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,19 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
 
+ char  _puts_recursion(char s[])
+{
+    printf("%s", s);
+
+}
+
+
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}

1961a5471a88b7e7e9827a3a92f87192ec51adc1
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-10-03 22:07:51 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,1 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that the number of arguments passed into it
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
 

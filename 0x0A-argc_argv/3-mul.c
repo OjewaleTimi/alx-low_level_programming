17a54a496df628b758e63dd392b5fc71e1076c4c
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-10-04 19:25:37 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,9 +16,10 @@ int main(int argc, char *argv[])
 {
    int a, b, result;
 
-   if( argc != 3) {
-    printf("Error\n");
-    return 1;
+   if( argc != 3) 
+   {
+   	 printf("Error\n");
+    	 return 1;
    }
 
    a = atoi(argv[1]);
@@ -27,5 +28,5 @@ int main(int argc, char *argv[])
 
    printf("%i", result);
 
-	return (0);
+   return (0);
 }

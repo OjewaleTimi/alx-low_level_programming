eb4c08cc4aabc81224567af622722edc66653099
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-10-04 19:22:28 +0100

    Create 3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that the number of arguments passed into it
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+   int a, b, result;
+
+   if( argc != 3) {
+    printf("Error\n");
+    return 1;
+   }
+
+   a = atoi(argv[1]);
+   b = atoi(argv[2]);
+   result = a * b;
+
+   printf("%i", result);
+
+	return (0);
+}

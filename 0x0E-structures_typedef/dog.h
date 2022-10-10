e3704e4e1270f58f4c28a904f92b9fe95f53df11
Ojewale Timilehin <108239539+OjewaleTimi@users.noreply.github.com>
2022-10-10 18:12:00 +0100

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+int main (int argc, char **argv)
+{
+
+  struct dog  
+     {
+    char *name;
+    float age;
+    char *owner;
+      };
+  
+    return 0;
+}
